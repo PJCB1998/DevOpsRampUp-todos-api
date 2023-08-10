aebebc7966b81a33055dbd54827a899268a94cf5
Pablo Calle <pjcb1998@hotmail.com>
2023-08-10 14:45:21 -0500

    Update todo-api-launcher.sh

--- todo-api-launcher.sh
+++ todo-api-launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-JWT_SECRET=PRFT TODO_API_PORT=8082 /usr/bin/npm start
+JWT_SECRET=PRFT TODO_API_PORT=8082 ZIPKIN_URL=http://192.168.56.80:9411/api/v2/spans /usr/bin/npm start
